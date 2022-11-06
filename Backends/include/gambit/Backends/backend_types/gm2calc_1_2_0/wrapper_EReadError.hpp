be59af56cf137e90b095313fc82938bfc98d73ba
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2022-11-06 02:24:28 +0100

    Deleted a bunch of files and folders that gambit_light doesn't care about.

--- Backends/include/gambit/Backends/backend_types/gm2calc_1_2_0/wrapper_EReadError.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __wrapper_EReadError_gm2calc_1_2_0_hpp__
-#define __wrapper_EReadError_gm2calc_1_2_0_hpp__
-
-
-#include "wrapper_EReadError_decl.hpp"
-#include "wrapper_EReadError_def.hpp"
-
-
-#endif /* __wrapper_EReadError_gm2calc_1_2_0_hpp__ */
