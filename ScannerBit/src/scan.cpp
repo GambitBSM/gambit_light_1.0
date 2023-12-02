4cd4e7fcbc876d7b2480d616a4f1e9add879591a
anderkve <null>
2023-12-02 11:09:36 +1000

    🔄 Synced local 'ScannerBit/' with remote 'ScannerBit/'

--- ScannerBit/src/scan.cpp
+++ ScannerBit/src/scan.cpp
@@ -65,12 +65,12 @@ namespace Gambit
                     if (options.hasKey("objectives") && options.hasKey("objectives", *it))
                     {
                         std::string plugin_name;
-                        if (options.hasKey("objectives", *it, "purpose") && options.hasKey("objectives", *it, "plugin"))
+                        if (options.hasKey("objectives", *it, "purpose") && (options.hasKey("objectives", *it, "plugin" )))
                         {
                             std::vector <std::string> purposes = get_yaml_vector<std::string>(options.getNode("objectives", *it, "purpose"));
                             plugin_name = options.getValue<std::string>("objectives", *it, "plugin");
                             for (auto it2 = purposes.begin(), end = purposes.end(); it2 != end; it2++)
-                                    names[*it2].push_back(std::pair<std::string, std::string>(*it, plugin_name));
+                                names[*it2].push_back(std::pair<std::string, std::string>(*it, plugin_name));
                         }
                         else
                         {
@@ -127,7 +127,8 @@ namespace Gambit
                 prior = new Gambit::Priors::CompositePrior(main_node["Parameters"], main_node["Priors"]);
                 factory = factoryIn;
                 Plugins::plugin_info.printer_prior(*printerInterface, *prior);
-            }   
+            }
+            
         }
         
         int Scan_Manager::Run()
@@ -211,12 +212,13 @@ namespace Gambit
         
         Scan_Manager::~Scan_Manager()
         {
-            if (has_local_factory)
+            if (has_local_factory && factory != 0)
             {
                 delete factory;
             }
             
-            delete prior;
+            if (prior != 0)
+                delete prior;
         }
     }
 }
